7282d96cc79a50f3f3a480977d85e0e040412a12
amanos1022 <angelo@angelomanos.com>
2016-01-14 12:39:56 -0800

    wired up validation for lead form

---
+++ modules/leadForm/main.js
@@ -0,0 +1,26 @@
+require(['../Utilities/AjaxForm', 'env', '../../vendor/lodash/lodash'], function(AjaxForm, env, _){
+	var form = $('#lead-form');
+	var af = new AjaxForm({
+		formElem 	: 	form,
+		url 		: 	env.apiUrl+'lead',
+		valid 		: 	function(r){
+			alert('Thank you! Someone will contact you soon.');
+			window.location.reload();
+		},
+		invalid 	: function(r){
+			_.forEach(r.messages, function(value, key){
+				form.find('[name="'+key+'"] + .error').html(value);
+			});
+		},
+		prePost 	: function(){
+			fields = {
+				first_name:'',
+				last_name:'',
+				email:''
+			}
+			_.forEach(fields, function(value, key){
+				form.find('[name="'+key+'"] + .error').html('');
+			});
+		}
+	});
+});
